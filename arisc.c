729090febfc9c7c020fc310573d4334d70fee4b5
MX_Master <mikhail@vydrenko.ru>
2020-04-19 20:06:57 +0600

    + gpio data get/set + spinlocks - unused info - unused code

--- arisc.c
+++ arisc.c
@@ -1,11 +1,3 @@
-/**
- * @file    arisc.c
- *
- * @brief   ARISC firmware API source
- *
- * This test program implements an API to ARISC firmware
- */
-
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
@@ -20,10 +12,6 @@
 
 
 
-// private vars
-
-#define TEST 0 // 0 = real execution of ARISC functions, 1 = just a safe test
-
 static uint32_t *shm_vrt_addr, *gpio_vrt_addr, *r_gpio_vrt_addr;
 static char *app_name = 0;
 volatile uint32_t * gpio[GPIO_PORTS_CNT] = {0};
@@ -31,45 +19,45 @@ volatile uint32_t * gpio_shm_set[GPIO_PORTS_CNT] = {0};
 volatile uint32_t * gpio_shm_clr[GPIO_PORTS_CNT] = {0};
 volatile uint32_t * gpio_shm_out[GPIO_PORTS_CNT] = {0};
 volatile uint32_t * gpio_shm_inp[GPIO_PORTS_CNT] = {0};
+volatile uint32_t * gpiod[GPIO_PORTS_CNT] = {0};
 
 
 
 
-// public methods
+static inline
+void gpio_spin_lock()
+{
+    while ( *gpiod[GPIO_ARISC_LOCK] );
+    *gpiod[GPIO_ARM_LOCK] = 1;
+}
 
-/**
- * @brief   set pin mode to OUTPUT
- * @param   port    GPIO port number    (0 .. GPIO_PORTS_CNT-1)
- * @param   pin     GPIO pin number     (0 .. GPIO_PINS_CNT-1)
- * @retval  none
- */
+static inline
+void gpio_spin_unlock()
+{
+    *gpiod[GPIO_ARM_LOCK] = 0;
+}
+
+static inline
 void gpio_pin_setup_for_output(uint32_t port, uint32_t pin)
 {
     if ( port >= GPIO_PORTS_CNT ) return;
     if ( pin >= GPIO_PINS_CNT ) return;
+    gpio_spin_lock();
     *gpio_shm_out[port] |= (1UL << pin);
+    gpio_spin_unlock();
 }
 
-/**
- * @brief   set pin mode to INPUT
- * @param   port    GPIO port number    (0 .. GPIO_PORTS_CNT-1)
- * @param   pin     GPIO pin number     (0 .. GPIO_PINS_CNT-1)
- * @retval  none
- */
+static inline
 void gpio_pin_setup_for_input(uint32_t port, uint32_t pin)
 {
     if ( port >= GPIO_PORTS_CNT ) return;
     if ( pin >= GPIO_PINS_CNT ) return;
+    gpio_spin_lock();
     *gpio_shm_inp[port] |= (1UL << pin);
+    gpio_spin_unlock();
 }
 
-/**
- * @brief   get pin state
- * @param   port    GPIO port number    (0 .. GPIO_PORTS_CNT-1)
- * @param   pin     GPIO pin number     (0 .. GPIO_PINS_CNT-1)
- * @retval  1 (HIGH)
- * @retval  0 (LOW)
- */
+static inline
 uint32_t gpio_pin_get(uint32_t port, uint32_t pin)
 {
     if ( port >= GPIO_PORTS_CNT ) return 0;
@@ -77,78 +65,68 @@ uint32_t gpio_pin_get(uint32_t port, uint32_t pin)
     return *gpio[port] & (1UL << pin) ? HIGH : LOW;
 }
 
-/**
- * @brief   set pin state to HIGH (1)
- * @param   port    GPIO port number    (0 .. GPIO_PORTS_CNT-1)
- * @param   pin     GPIO pin number     (0 .. GPIO_PINS_CNT-1)
- * @retval  none
- */
+static inline
 void gpio_pin_set(uint32_t port, uint32_t pin)
 {
     if ( port >= GPIO_PORTS_CNT ) return;
     if ( pin >= GPIO_PINS_CNT ) return;
+    gpio_spin_lock();
     *gpio_shm_set[port] |= (1UL << pin);
+    gpio_spin_unlock();
 }
 
-/**
- * @brief   set pin state to LOW (0)
- * @param   port    GPIO port number    (0 .. GPIO_PORTS_CNT-1)
- * @param   pin     GPIO pin number     (0 .. GPIO_PINS_CNT-1)
- * @retval  none
- */
+static inline
 void gpio_pin_clear(uint32_t port, uint32_t pin)
 {
     if ( port >= GPIO_PORTS_CNT ) return;
     if ( pin >= GPIO_PINS_CNT ) return;
+    gpio_spin_lock();
     *gpio_shm_clr[port] |= (1UL << pin);
+    gpio_spin_unlock();
 }
 
-/**
- * @brief   get port state
- * @param   port    GPIO port number (0 .. GPIO_PORTS_CNT-1)
- * @note    each bit value of returned value represents port pin state
- * @retval  0 .. 0xFFFFFFFF
- */
+static inline
 uint32_t gpio_port_get(uint32_t port)
 {
     if ( port >= GPIO_PORTS_CNT ) return 0;
     return *gpio[port];
 }
 
-/**
- * @brief   set port pins state by mask
- *
- * @param   port    GPIO port number        (0 .. GPIO_PORTS_CNT-1)
- * @param   mask    GPIO pins mask to set   (0 .. 0xFFFFFFFF) \n\n
- *                  mask examples: \n\n
- *                      mask = 0xFFFFFFFF (0b11111111111111111111111111111111) means <b>set all pins state to 1 (HIGH)</b> \n
- *                      mask = 0x00000001 (0b1) means <b>set pin 0 state to 1 (HIGH)</b> \n
- *                      mask = 0x0000000F (0b1111) means <b>set pins 0,1,2,3 states to 1 (HIGH)</b>
- *
- * @retval  none
- */
+static inline
 void gpio_port_set(uint32_t port, uint32_t mask)
 {
     if ( port >= GPIO_PORTS_CNT ) return;
+    gpio_spin_lock();
     *gpio_shm_set[port] = mask;
+    gpio_spin_unlock();
 }
 
-/**
- * @brief   clear port pins state by mask
- *
- * @param   port    GPIO port number        (0 .. GPIO_PORTS_CNT-1)
- * @param   mask    GPIO pins mask to clear (0 .. 0xFFFFFFFF) \n\n
- *                  mask examples: \n\n
- *                  mask = 0xFFFFFFFF (0b11111111111111111111111111111111) means <b>set all pins state to 0 (LOW)</b> \n
- *                  mask = 0x00000003 (0b11) means <b>set pins 0,1 states to 0 (LOW)</b> \n
- *                  mask = 0x00000008 (0b1000) means <b>set pin 3 state to 0 (LOW)</b>
- *
- * @retval  none
- */
+static inline
 void gpio_port_clear(uint32_t port, uint32_t mask)
 {
     if ( port >= GPIO_PORTS_CNT ) return;
+    gpio_spin_lock();
     *gpio_shm_clr[port] = mask;
+    gpio_spin_unlock();
+}
+
+static inline
+void gpio_data_set(uint32_t name, uint32_t value)
+{
+    if ( name >= GPIO_DATA_CNT ) return;
+    gpio_spin_lock();
+    *gpiod[name] = value;
+    gpio_spin_unlock();
+}
+
+static inline
+uint32_t gpio_data_get(uint32_t name)
+{
+    if ( name >= GPIO_DATA_CNT ) return 0;
+    gpio_spin_lock();
+    uint32_t value = *gpiod[name];
+    gpio_spin_unlock();
+    return value;
 }
 
 
@@ -163,6 +141,7 @@ void mem_init(void)
     mem_fd = open("/dev/mem", O_RDWR|O_SYNC);
     if ( mem_fd  < 0 ) { printf("ERROR: can't open /dev/mem file\n"); return; }
 
+    // mmap shmem
     addr = ARISC_SHM_BASE & ~(4096 - 1);
     off = ARISC_SHM_BASE & (4096 - 1);
     shm_vrt_addr = mmap(NULL, ARISC_SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, mem_fd, addr);
@@ -174,7 +153,12 @@ void mem_init(void)
         gpio_shm_out[port] = (uint32_t *) ( shm_vrt_addr + (off + port*4 + (GPIO_SHM_OUT_BASE - GPIO_SHM_BASE))/4 );
         gpio_shm_inp[port] = (uint32_t *) ( shm_vrt_addr + (off + port*4 + (GPIO_SHM_INP_BASE - GPIO_SHM_BASE))/4 );
     }
+    for ( port = 0; port < GPIO_DATA_CNT; port++ )
+    {
+        gpiod[port] = (uint32_t *) ( shm_vrt_addr + (off + port*4 + (GPIO_SHM_DATA_BASE - GPIO_SHM_BASE))/4 );
+    }
 
+    // mmap gpio
     addr = GPIO_BASE & ~(4096 - 1);
     off = GPIO_BASE & (4096 - 1);
     gpio_vrt_addr = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, mem_fd, addr);
@@ -184,6 +168,7 @@ void mem_init(void)
         gpio[port] = (uint32_t *) ( gpio_vrt_addr + (off + port*GPIO_BANK_SIZE + 16)/4 );
     }
 
+    // mmap r_gpio (PL)
     addr = GPIO_R_BASE & ~(4096 - 1);
     off = GPIO_R_BASE & (4096 - 1);
     r_gpio_vrt_addr = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_SHARED, mem_fd, addr);
@@ -274,7 +259,6 @@ int32_t reg_match(const char *source, const char *pattern, uint32_t *match_array
             // if we have an unknown string
             else
             {
-//                printf("    unknown argument string `%s` \n", (const char *)&match[0]);
                 ret = 4;
                 break;
             }
@@ -294,7 +278,7 @@ int32_t parse_and_exec(const char *str)
 {
     uint32_t arg[10] = {0};
 
-    #define UINT " *([0-9]+|0x[A-Fa-f]+|0b[01]+|P[ABCDEFGL]|PH_[ABZ]|PHASE_[ABZ]) *"
+    #define UINT " *([0-9]+|0x[A-Fa-f]+|0b[01]+|P[ABCDEFGL]) *"
     #define INT " *(\\-?[0-9]+) *"
 
     // --- HELP, EXAMPLES, EXIT ------
@@ -313,55 +297,34 @@ int32_t parse_and_exec(const char *str)
     %s \"function1(param1, param2, ..)\" \"function2(param1, param2, ..)\" ...\n\
 \n\
     %s help         show help info \n\
-    %s examples     show a few examples \n\
     %s exit|quit|q  program quit \n\
 \n\
   Functions: \n\
 \n\
          gpio_pin_setup_for_output  (port, pin) \n\
          gpio_pin_setup_for_input   (port, pin) \n\
-    int  gpio_pin_get               (port, pin) \n\
+    u32  gpio_pin_get               (port, pin) \n\
          gpio_pin_set               (port, pin) \n\
          gpio_pin_clear             (port, pin) \n\
-    int  gpio_port_get              (port) \n\
+    u32  gpio_port_get              (port) \n\
          gpio_port_set              (port, mask) \n\
          gpio_port_clear            (port, mask) \n\
-\n\
+    u32  gpio_data_get              (name) \n\
+         gpio_data_set              (name, value) \n\
 \n\
   Legend: \n\
 \n\
-    port            GPIO port (0..%u, PA, PB, PC, PD, PE, PF, PG, PL)\n\
-    pin             GPIO pin (0..%u)\n\
-    mask            GPIO pins mask (0b0 .. 0b11111111111111111111111111111111)\n\
+    port    GPIO port (0..%u | PA/PB/PC/PD/PE/PF/PG/PL)\n\
+    pin     GPIO pin (0..%u)\n\
+    mask    GPIO pins mask (u32)\n\
+    name    GPIO data name (0..%u)\n\
+    value   GPIO data value (u32)\n\
 \n\
   NOTE:\n\
     If you are using stdin/stdout mode, omit `%s` and any \" brackets\n\
 \n",
-            app_name, app_name, app_name, app_name,
-            (GPIO_PORTS_CNT - 1), (GPIO_PINS_CNT - 1),
-            app_name
-        );
-        return 0;
-    }
-
-    if ( !reg_match(str, "example", &arg[0], 0) )
-    {
-        printf(
-"\n\
-  GPIO examples:\n\
-\n\
-    %s \"gpio_pin_setup_for_output(PA,15)\" # setup PA15 pin as output \n\
-    %s \"gpio_pin_clear(PA,15)\"            # set PA15 state to 0 \n\
-    %s \"gpio_pin_set(PA,15)\"              # set PA15 state to 1 \n\
-    %s \"gpio_pin_get(PA,15)\"              # get PA15 state \n\
-    %s \"gpio_port_clear(PA, 0b11)\"        # set PA0,PA1 state to 0 \n\
-    %s \"gpio_port_set(PA, 0b1011)\"        # set PA0,PA1,PA3 state to 1 \n\
-    %s \"gpio_port_get(PA)\"                # get PA port all pins state \n\
-\n\
-  NOTE:\n\
-    If you are using stdin/stdout mode, omit `%s` and any \" brackets\n\
-\n",
-            app_name, app_name, app_name, app_name, app_name, app_name, app_name,
+            app_name, app_name, app_name,
+            (GPIO_PORTS_CNT - 1), (GPIO_PINS_CNT - 1), (GPIO_DATA_CNT - 1),
             app_name
         );
         return 0;
@@ -371,79 +334,72 @@ int32_t parse_and_exec(const char *str)
 
     if ( !reg_match(str, "gpio_pin_setup_for_output *\\("UINT","UINT"\\)", &arg[0], 2) )
     {
-#if !TEST
         gpio_pin_setup_for_output(arg[0], arg[1]);
-#endif
         printf("OK\n");
         return 0;
     }
 
     if ( !reg_match(str, "gpio_pin_setup_for_input *\\("UINT","UINT"\\)", &arg[0], 2) )
     {
-#if !TEST
         gpio_pin_setup_for_input(arg[0], arg[1]);
-#endif
         printf("OK\n");
         return 0;
     }
 
     if ( !reg_match(str, "gpio_pin_set *\\("UINT","UINT"\\)", &arg[0], 2) )
     {
-#if !TEST
         gpio_pin_set(arg[0], arg[1]);
-#endif
         printf("OK\n");
         return 0;
     }
 
     if ( !reg_match(str, "gpio_pin_clear *\\("UINT","UINT"\\)", &arg[0], 2) )
     {
-#if !TEST
         gpio_pin_clear(arg[0], arg[1]);
-#endif
         printf("OK\n");
         return 0;
     }
 
     if ( !reg_match(str, "gpio_pin_get *\\("UINT","UINT"\\)", &arg[0], 2) )
     {
-#if !TEST
         printf("%u\n", gpio_pin_get(arg[0], arg[1]));
-#else
-        printf("%u\n", 1);
-#endif
         return 0;
     }
 
     if ( !reg_match(str, "gpio_port_set *\\("UINT","UINT"\\)", &arg[0], 2) )
     {
-#if !TEST
         gpio_port_set(arg[0], arg[1]);
-#endif
         printf("OK\n");
         return 0;
     }
 
     if ( !reg_match(str, "gpio_port_clear *\\("UINT","UINT"\\)", &arg[0], 2) )
     {
-#if !TEST
         gpio_port_clear(arg[0], arg[1]);
-#endif
         printf("OK\n");
         return 0;
     }
 
     if ( !reg_match(str, "gpio_port_get *\\("UINT"\\)", &arg[0], 1) )
     {
-#if !TEST
         uint32_t s = gpio_port_get(arg[0]);
-#else
-        uint32_t s = 0x12345678;
-#endif
         uint32_t b = 32;
         printf("%u, 0x%X, 0b", s, s);
         for ( ; b--; ) printf("%u", (s & (1U << b) ? 1 : 0));
         printf("\n");
+        return 0;
+    }
+
+    if ( !reg_match(str, "gpio_data_get *\\("UINT"\\)", &arg[0], 1) )
+    {
+        printf("%u\n", gpio_data_get(arg[0]));
+        return 0;
+    }
+
+    if ( !reg_match(str, "gpio_data_set *\\("UINT","UINT"\\)", &arg[0], 2) )
+    {
+        gpio_data_set(arg[0], arg[1]);
+        printf("OK\n");
         return 0;
     }
 
@@ -474,10 +430,9 @@ int main(int argc, char *argv[])
         char input_str[255] = {0};
 
         printf("\n\
-  Welcome to stdin/stdout mode of ARISC CNC API.\n\
+  Welcome to stdin/stdout mode of ARISC CNC CLI.\n\
 \n\
   Type `help` to see help info.\n\
-  Type `examples` to see working examples.\n\
   Type `q`|`quit`|`exit` to quit the program.\n\
             \n");
 
--- arisc.h
+++ arisc.h
@@ -1,11 +1,3 @@
-/**
- * @file    arisc.h
- *
- * @brief   ARISC firmware API header
- *
- * This test program implements an API to ARISC firmware
- */
-
 #ifndef _ARISC_H
 #define _ARISC_H
 
@@ -19,24 +11,31 @@
 #define ARISC_SHM_SIZE          (4096)
 #define ARISC_SHM_BASE          (ARISC_FW_BASE + ARISC_FW_SIZE - ARISC_SHM_SIZE)
 
-#define GPIO_BASE               0x01C20800 // GPIO registers block start address
-#define GPIO_R_BASE             0x01f02c00 // GPIO R registers block start address
+#define GPIO_BASE               0x01c20800
+#define GPIO_R_BASE             0x01f02c00
 #define GPIO_BANK_SIZE          0x24
 
-#define GPIO_PORTS_CNT          8   // number of GPIO ports
-#define GPIO_PINS_CNT           24  // number of GPIO port pins
+#define GPIO_PORTS_CNT          8
+#define GPIO_PINS_CNT           24
+
+enum
+{
+    GPIO_USED,
+    GPIO_ARM_LOCK,
+    GPIO_ARISC_LOCK,
+    GPIO_PORT_MAX_ID,
+    GPIO_DATA_CNT
+};
 
 #define GPIO_SHM_BASE           (ARISC_SHM_BASE)
 #define GPIO_SHM_SET_BASE       (GPIO_SHM_BASE     + 0)
 #define GPIO_SHM_CLR_BASE       (GPIO_SHM_SET_BASE + GPIO_PORTS_CNT*4)
 #define GPIO_SHM_OUT_BASE       (GPIO_SHM_CLR_BASE + GPIO_PORTS_CNT*4)
 #define GPIO_SHM_INP_BASE       (GPIO_SHM_OUT_BASE + GPIO_PORTS_CNT*4)
-#define GPIO_SHM_SIZE           (GPIO_SHM_INP_BASE + GPIO_PORTS_CNT*4 - GPIO_SHM_BASE)
+#define GPIO_SHM_DATA_BASE      (GPIO_SHM_INP_BASE + GPIO_PORTS_CNT*4)
+#define GPIO_SHM_SIZE           (GPIO_SHM_DATA_BASE + GPIO_DATA_CNT*4 - GPIO_SHM_SET_BASE)
 
-// GPIO port names
 enum { PA, PB, PC, PD, PE, PF, PG, PL };
-
-// GPIO pin states
 enum { LOW, HIGH };
 
 
