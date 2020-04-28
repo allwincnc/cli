6ffb47dcc432004b6a19af83a8834d645f14787c
MX_Master <mikhail@vydrenko.ru>
2020-04-28 13:25:04 +0600

    * pulses calc fix

--- arisc.c
+++ arisc.c
@@ -345,8 +345,8 @@ int32_t stepgen_task_add(uint32_t c, int32_t pulses, uint32_t time, uint32_t saf
 
     if ( *_pgc[c][s][PG_TOGGLES] && !*_pgc[c][s][PG_TYPE] )
     {
-        uint32_t tgs = *_pgc[c][s][PG_TOGGLES] - 1;
-        *_pgc[c][s][PG_TOGGLES] = 1 + tgs%2;
+        uint32_t tgs = (*_pgc[c][s][PG_TOGGLES] / 2) * 2;
+        *_pgc[c][s][PG_TOGGLES] -= tgs;
         stp_tgs += tgs;
         s = (s+1) & PG_CH_SLOT_MAX;
     }
